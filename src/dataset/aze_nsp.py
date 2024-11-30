d11986ff8f4d2c6090a2f0e5a0f7474208041153
Eyvaz Najafli <50397370+Eyvaz27@users.noreply.github.com>
2024-11-30 18:29:26 +0000

    finalizing dataset

--- src/dataset/aze_nsp.py
+++ src/dataset/aze_nsp.py
@@ -4,8 +4,11 @@ import pandas as pd
 from dataclasses import dataclass
 from torch.utils.data import Dataset
 from typing import Union, Optional, Literal
-from .meta_info import Stage, DATASET_PATH
+from tokenizers import Tokenizer, models, pre_tokenizers, decoders, trainers, processors
+from .meta_info import Stage, DATASET_PATH, TokenizerCfg
+from sklearn.model_selection import train_test_split
 from datasets import load_from_disk
+from tokenizers import Tokenizer
 
 @dataclass
 class AZE_NSP_DatasetCfg:
@@ -15,6 +18,7 @@ class AZE_NSP_DatasetCfg:
     truncation: bool
     embedding_dim: int
     eval_set_ratio: float
+    tokenizer: TokenizerCfg
 
 class AZE_NSP_Dataset(Dataset):
     def __init__(self,
@@ -35,64 +39,65 @@ class AZE_NSP_Dataset(Dataset):
     def load_text_samples(self):
 
         aze_nsp_dataset = load_from_disk(dataset_path=DATASET_PATH)
-        from sklearn.model_selection import train_test_split
-
         raw_data = aze_nsp_dataset["train"].to_pandas()
         X = raw_data["partial_text"].map(lambda text: text.lower())
         y = raw_data["gold_ending"].map(lambda text: text.lower())
-        X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+        X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=self.cfg.eval_set_ratio, random_state=42)
 
         if self.stage == "test":
             X_test.reset_index(inplace=True, drop=True)
             y_test.reset_index(inplace=True, drop=True)
-
             self.input_text_samples = X_test.map(lambda sentence: "<bos> " + sentence + " <eos>")
             self.output_text_samples = y_test.map(lambda sentence: "<bos> " + sentence + " <eos>")
-            
-
-        X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.1/0.9, random_state=42)
-        X_train_val.reset_index(inplace=True, drop=True)
-        X_train.reset_index(inplace=True, drop=True)
         
-        X_val.reset_index(inplace=True, drop=True)
-        y_train_val.reset_index(inplace=True, drop=True)
-        y_train.reset_index(inplace=True, drop=True)
-        
-        y_val.reset_index(inplace=True, drop=True)
-
-        
-        else: raw_csv_file = pd.read_csv(TRAIN_FILE_PATH, encoding = 'latin1')
-        
-        AZE_NSP__lower = lambda AZE_NSP_: AZE_NSP_.lower()
-        feature_texts = raw_csv_file["OriginalAZE_NSP_"].map(AZE_NSP__lower)
-        label_texts = raw_csv_file["Sentiment"].map(CLASS_MAP)
-
-        if self.stage == "test":
-            self.feature_texts = feature_texts
-            self.label_texts = label_texts
         else:
-            np.random.seed(seed=self.seed)
-            random_perm_indexes = np.random.permutation(len(feature_texts))
-            train_sample_indexes = random_perm_indexes[:int((1.0 - self.eval_set_ratio) * len(random_perm_indexes))]
-            validation_sample_indexes = random_perm_indexes[int((1.0 - self.eval_set_ratio) * len(random_perm_indexes)):]
+            validation_ratio = self.cfg.eval_set_ratio / (1.0 - self.cfg.eval_set_ratio)
+            X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=validation_ratio, random_state=42)
+            X_train_val.reset_index(inplace=True, drop=True)
+            X_train.reset_index(inplace=True, drop=True)
+            X_val.reset_index(inplace=True, drop=True)
+            y_train_val.reset_index(inplace=True, drop=True)
+            y_train.reset_index(inplace=True, drop=True)
+            y_val.reset_index(inplace=True, drop=True)
+
+            if not os.path.isfile(str(self.cfg.tokenizer.ckpt_path))
+                self.full_input_set = X_train_val.map(lambda sentence: "<bos> " + sentence + " <eos>")
             if self.stage == "train":
-                self.feature_texts = feature_texts[train_sample_indexes]
-                self.label_texts = label_texts[train_sample_indexes]
-            elif self.stage == "validation":
-                self.feature_texts = feature_texts[validation_sample_indexes]
-                self.label_texts = label_texts[validation_sample_indexes]
-        # # # below code is important 
-        self.feature_texts.reset_index(inplace=True, drop=True)
-        self.label_texts.reset_index(inplace=True, drop=True)
+                self.input_text_samples = X_train.map(lambda sentence: "<bos> " + sentence + " <eos>")
+                self.output_text_samples = y_train.map(lambda sentence: "<bos> " + sentence + " <eos>")
+            else:
+                self.input_text_samples = X_val.map(lambda sentence: "<bos> " + sentence + " <eos>")
+                self.output_text_samples = y_val.map(lambda sentence: "<bos> " + sentence + " <eos>")
     
     def init_tokenizer(self):
-        self.auto_tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
-        self.tokenizer = lambda AZE_NSP_: self.auto_tokenizer(AZE_NSP_, padding=self.cfg.padding, truncation=self.cfg.truncation,
-                                                           max_length=self.cfg.max_length, return_tensors="pt")
-        self.bert_model = BertModel.from_pretrained("bert-base-uncased", 
-                                                    torch_dtype=torch.float32, # because network needs fp32
-                                                    attn_implementation="sdpa").eval()
-        self.embedding_layer = lambda AZE_NSP__tokens: self.bert_model.embeddings(AZE_NSP__tokens['input_ids'])
+        if os.path.isfile(str(self.cfg.tokenizer.ckpt_path)):
+            self.tokenizer = Tokenizer.from_file(self.cfg.tokenizer.ckpt_path)
+        else: self.fit_tokenizer()
+
+        self.tokenizer.enable_padding(direction=self.cfg.padding)
+        self.tokenizer.enable_truncation(max_length=self.cfg.max_length, 
+                                         direction=self.cfg.truncation)
+
+    def fit_tokenizer(self):
+        # Initialize a tokenizer
+        self.tokenizer = Tokenizer(models.BPE())
+        # Customize pre-tokenization and decoding
+        self.tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=True)
+        self.tokenizer.decoder = decoders.ByteLevel()
+        self.tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
+        # And then train
+        initial_aze_alphabet = list(set(' '.join(X_train_val)))
+        
+        # defining parameters for BPE
+        trainer = trainers.BpeTrainer(
+            vocab_size=self.cfg.tokenizer.vocab_size, 
+            min_frequency=self.cfg.tokenizer.min_frequency,
+            initial_alphabet=initial_aze_alphabet,
+            special_tokens=["<bos>", "<eos>", "<pad>", "<mask>"])
+
+        # training tokenizer on a given Train/Val set
+        self.tokenizer.train_from_iterator(self.full_input_set, trainer=trainer)
+        self.tokenizer.save(self.cfg.tokenizer.ckpt_path, pretty=True)
         
     def __len__(self):
         return len(self.feature_texts)
--- src/dataset/meta_info.py
+++ src/dataset/meta_info.py
@@ -1,5 +1,12 @@
-from typing import Literal
+from typing import Literal, Optional
+from dataclasses import dataclass
 
 Stage = Literal["train", "validation", "test"]
 
 DATASET_PATH = "/workspaces/Implementation-of-Neural-Machine-Translation-of-Rare-Words-with-Subword-Units-on-Azerbaijani/data_source"
+
+@dataclass
+class TokenizerCfg:
+    vocab_size: int
+    min_frequency: int
+    ckpt_path: Optional[str]
