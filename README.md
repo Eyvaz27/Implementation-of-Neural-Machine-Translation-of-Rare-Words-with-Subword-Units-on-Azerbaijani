d5e74259b6c985def11825d2c5d7e841d2d1e4e4
Eyvaz Najafli <50397370+Eyvaz27@users.noreply.github.com>
2024-12-01 12:19:11 +0000

    finalized

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Implementation-of-Neural-Machine-Translation-of-Rare-Words-with-Subword-Units-on-Azerbaijani
 This project is designed to apply Neural Machine Translation techniques proposed in https://arxiv.org/abs/1508.07909 on Azerbaijani language for NSP task
 
+How to use the code base:
+- Install https://huggingface.co/datasets/allmalab/AZE-NSP dataset into /data_source/ directory
+- Run huggingface_tokenizers.ipynb in order to create a tokenizer on Azerbaijani language
+- Run huffman_trees.ipynb in order to create a huffman tree that can be used by decoder
+- Follow instruction below after modifying "/config" directory with the preferred settings
 
 How to install libraries:
 - for extra libraries: python -m pip install --no-cache-dir <package_name>
--- src/model/decoder/decoder_lstmCell.py
+++ src/model/decoder/decoder_lstmCell.py
@@ -49,6 +49,7 @@ class DecoderLSTMCell(Decoder[DecoderLSTMCellCfg]):
                 nn.Linear(in_features=self.cfg.input_size, 
                         out_features=self.cfg.vocab_size, bias=True), nn.LogSoftmax(dim=1))
         self.out_act = nn.ReLU()
+
     def init_hidden_cell(self):
         self.y0 = torch.nn.Parameter(torch.randn(1, self.cfg.input_size)) # requires_grad
         self.h0 = torch.nn.Parameter(torch.randn(1, self.cfg.hidden_size)) # requires_grad
@@ -73,8 +74,9 @@ class DecoderLSTMCell(Decoder[DecoderLSTMCellCfg]):
              hx_seq = repeat(hx, "n dim -> n s dim", s=seq)
              attn_input = torch.concatenate((features, hx_seq), dim=-1)       # [N, seq, hidden_dim + feature_dim]
              alignment_weights = torch.exp(self.alignment_model(attn_input))  # [N, seq, 1]
+             alignment_weights = alignment_weights / torch.sum(alignment_weights, dim=1, keepdim=True)
              context = torch.sum(alignment_weights * features, dim=1)         # [N dim] -> summing over T_x
-             
+
              # Below we have modelled [y_{t-1}; context] combination first and then apply RNN
              context_plus_input = self.context_agg(torch.concatenate((context, outputs[-1]), dim=-1))
              hx, cx = self.cell(context_plus_input, (hx, cx))  # running LSTM cell over sequence
